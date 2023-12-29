94247f285437d9dc6871b6fb5106ad2a1b7517ec
Joachim Velde <kjvelde@ifi.uio.no>
2023-12-29 11:31:33 +0100

    cost function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 class NeuralNetwork {
     private:
@@ -14,8 +15,10 @@ class NeuralNetwork {
 
     public:
         NeuralNetwork(size_t arch[], size_t l, Matrix in, Matrix out);
-        void print();
         void forward();
+        float cost();
+        void backprop();
+        void print();
 };
 
 NeuralNetwork::NeuralNetwork(size_t arch[], size_t layers, Matrix in, Matrix out) : layers(layers), train_out(out) {
@@ -33,10 +36,26 @@ NeuralNetwork::NeuralNetwork(size_t arch[], size_t layers, Matrix in, Matrix out
 
 void NeuralNetwork::forward() {
     for (size_t i = 1; i < layers; i++) {
-        std::cout << "multiplying " << as[i - 1].get_rows() << "x" << as[i - 1].get_cols()
-            << " with " << ws[i - 1].get_rows() << "x" << ws[i - 1].get_cols() << std::endl;
         as[i] = (as[i - 1] * ws[i - 1]) + bs[i - 1];
     }
+}
+
+float NeuralNetwork::cost() {
+    float c = 0;
+
+    Matrix out = as.back();
+
+    for (size_t i = 0; i < out.get_rows(); i++) {
+        c += pow((train_out.get(i, 0) - out.get(i, 0)), 2);
+    }
+
+    c /= out.get_rows();
+
+    return c;
+}
+
+void NeuralNetwork::backprop() {
+
 }
 
 void NeuralNetwork::print() {
@@ -61,21 +80,19 @@ int main()
     // Generating random floats
     srand(69);
 
-    Matrix in(3, 3);
-    Matrix out(2, 2);
+    Matrix in(4, 4);
+    Matrix out(4, 1);
     in.fill(3.14f);
 
-    size_t arch[] = { 3, 4, 2 };
-    NeuralNetwork nn(arch, 3, in, out);
-
-    // in.print();
-    // out.print();
-    // (in * out).print();
+    size_t arch[] = { 4, 4 };
+    NeuralNetwork nn(arch, 2, in, out);
 
     nn.print();
+    std::cout << std::endl << "cost = " << nn.cost() << std::endl;
     nn.forward();
     std::cout << std::endl << "---------- FOWARDING ----------" << std::endl << std::endl;
     nn.print();
+    std::cout << std::endl << "cost = " << nn.cost() << std::endl;
 
     return 0;
 }
