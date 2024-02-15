33db53cc917903493fbaabff8208ce3b560c1106
joachimvelde <42566158+joachimvelde@users.noreply.github.com>
2024-02-15 14:51:52 +0100

    Weights and biases should be initialized to small positive and negative values

--- ml.h
+++ ml.h
@@ -177,8 +177,8 @@ Network net_alloc(size_t layer_count, size_t layers[])
         n.bs[i-1] = mat_alloc(layers[i], 1);
         n.as[i] = mat_alloc(layers[i], 1);
 
-        mat_rand(n.ws[i-1], 0.0, 1.0);
-        mat_rand(n.bs[i-1], 0.0, 1.0);
+        mat_rand(n.ws[i-1], -1.0, 1.0);
+        mat_rand(n.bs[i-1], -1.0, 1.0);
     }
 
     return n;
